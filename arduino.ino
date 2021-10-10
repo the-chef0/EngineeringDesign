8159f15244211a6be1eb2e135e249b15ba9d8df9
Michal Kuchar <55955948+the-chef0@users.noreply.github.com>
2021-10-10 15:23:12 +0200

    Add files via upload

--- arduino.ino
+++ arduino.ino
@@ -1,27 +1,16 @@
 #include <Servo.h>
-#include <TouchScreen.h>
-#include <Adafruit_GFX.h> // Hardware-specific library
 #include <MCUFRIEND_kbv.h>
-#include <DS3231.h>
+#include <DS3232RTC.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 #include <Fonts/FreeSans18pt7b.h>
 #include <Fonts/FreeSans12pt7b.h>
 
-#define PN532_SCK  (13)
-#define PN532_MISO (12)
-#define PN532_MOSI (11)
-#define PN532_SS   (10)
-Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-
+Adafruit_PN532 nfc((13), (12), (11), (10));
 
 MCUFRIEND_kbv tft;
 Servo servo1;
-DS3231 clock;
-bool century = false;
-bool h12Flag;
-bool pmFlag;
 
 int screenWidth = 320;
 int screenHeight = 240;
@@ -30,32 +19,25 @@ int16_t  x1, y1;
 uint16_t w, h;
 int prevSecond;
 
-int scheduleYears[] =  {21,21,21,21,21};
-int scheduleMonths[] = {10,10,10,10,10};
-int scheduleDays[] =   {4,4,4,4,4};
-int scheduleHours[] =  {11,11,11,12,12};
-int scheduleMinutes[] = {48,50,55,0,5};
+int scheduleYears[] =  {21, 21, 21, 21, 21};
+int scheduleMonths[] = {10, 10, 10, 10, 10};
+int scheduleDays[] =   {10, 10, 10, 10, 10};
+int scheduleHours[] =  {15, 15, 15, 15, 15};
+int scheduleMinutes[] = {30, 35, 40, 45, 50};
 int counter = 0;
 bool doseAvailable = false;
 
 void setup() {
-  servo1.attach(9);
+  setSyncProvider(RTC.get);
+  //servo1.attach(9);
   rotate();
   Serial.begin(9600);
-  tft.reset();
   uint16_t identifier = tft.readID();
   if (identifier == 0xEFEF) identifier = 0x9486;
-  
+
   tft.begin(identifier);
   tft.setRotation(3);
-  tft.fillScreen(0x0);
   tft.setFont(&FreeSans18pt7b);
-
-  //clock.setMinute(34);
-  //clock.setHour(11);
-  //clock.setDate(4);
-  //clock.setMonth(10);
-  //clock.setYear(21);
 
   nfc.begin();
   uint32_t versiondata = nfc.getFirmwareVersion();
@@ -73,31 +55,31 @@ void setup() {
 }
 
 void loop() {
-  int currYear = clock.getYear();
-  int currMonth = clock.getMonth(century);
-  int currDay = clock.getDate();
-  int currHour = clock.getHour(h12Flag, pmFlag);
-  int currMinute = clock.getMinute();
-  int currSecond = clock.getSecond();
-  
+  int currYear = year();
+  int currMonth = month();
+  int currDay = day();
+  int currHour = hour();
+  int currMinute = minute();
+  int currSecond = second();
+
   if (currSecond != prevSecond) {
-    
+
     int scheduleYear = scheduleYears[counter];
     int scheduleMonth = scheduleMonths[counter];
     int scheduleDay = scheduleDays[counter];
     int scheduleHour = scheduleHours[counter];
     int scheduleMinute = scheduleMinutes[counter];
-    
+
     if (currYear >= scheduleYear &
         currMonth >= scheduleMonth &
         currDay >= scheduleDay &
         currHour >= scheduleHour &
         currMinute >= scheduleMinute) {
-          
-          doseAvailable = true;
-          counter = counter + 1;
-          Serial.println("dose available");      
-        }
+
+      doseAvailable = true;
+      counter = counter + 1;
+      Serial.println("dose available");
+    }
 
     int cntdwnHours = scheduleHour - currHour;
     int cntdwnMinutes = scheduleMinute - currMinute - 1;
@@ -105,7 +87,7 @@ void loop() {
     String cntdwnHoursStr = "";
     String cntdwnMinutesStr = "";
     String cntdwnSecondsStr = "";
-    
+
     if (cntdwnHours < 10) {
       cntdwnHoursStr = "0" + String(cntdwnHours);
     } else {
@@ -123,16 +105,16 @@ void loop() {
     } else {
       cntdwnSecondsStr = String(cntdwnSeconds);
     }
-    
+
     String timeString = cntdwnHoursStr + ":" + cntdwnMinutesStr + ":" + cntdwnSecondsStr;
-    Serial.println(timeString);
+    updateDisplay(tft,timeString);
     prevSecond = currSecond;
   }
 
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 50);
 
   if (success) {
     if (doseAvailable) {
@@ -142,8 +124,7 @@ void loop() {
       doseAvailable = false;
     }
     else {
-      Serial.println("Next dose available in:");
-      delay(500);
+      Serial.println("Next dose not available yet.");
     }
   }
 }
@@ -157,14 +138,14 @@ void rotate() {
 void updateDisplay(MCUFRIEND_kbv screen, String string) {
   screen.fillScreen(0x0);
   screen.getTextBounds(string, 0, 0, &x1, &y1, &w, &h);
-  screen.setCursor(getCursorX(),getCursorY());
+  screen.setCursor(getCursorX(), getCursorY());
   screen.println(string);
 }
 
 int getCursorX() {
-  return (screenWidth - w)/2;
+  return (screenWidth - w) / 2;
 }
 
 int getCursorY() {
-  return (screenHeight/2) + (h/2);
+  return (screenHeight / 2) + (h / 2);
 }
